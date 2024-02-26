fc6265b27084f40ec9203da2ab015591c19745a9
Nabila Ayaba <nabilaayaba@gmail.com>
2024-02-26 16:51:14 -0500

    First glyph updates to chosen emoji

--- main.js
+++ main.js
@@ -6,13 +6,21 @@
 
 /*----- cached elements  -----*/
 const checkBtn = document.getElementById('check-btn')
-
+const glyph01 = document.getElementById('glyph01')
+const glyph01Options = document.querySelectorAll('.glyph01')
+console.log(glyph01Options)
 
 /*----- event listeners ------*/
 checkBtn.addEventListener('click', checkGuess)
-
+glyph01Options.forEach(option => {
+    option.addEventListener('click', () => updateGlyph(option))
+})
 
 /*-------- functions ---------*/
-function checkGuess () {
+function checkGuess() {
     console.log("Check guess function")
+}
+
+function updateGlyph(option) {
+    glyph01.innerHTML = option.innerHTML
 }
