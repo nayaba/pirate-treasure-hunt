f46491e7f8a936b80fa294516bd87f8369a0c46b
Nabila Ayaba <nabilaayaba@gmail.com>
2024-02-26 15:38:09 -0500

    Dropdown menu added for glyph01

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log("it works!")
+/*-------- constants ---------*/
+
+
+/*----- state variables ------*/
+
+
+/*----- cached elements  -----*/
+const checkBtn = document.getElementById('check-btn')
+
+
+/*----- event listeners ------*/
+checkBtn.addEventListener('click', checkGuess)
+
+
+/*-------- functions ---------*/
+function checkGuess () {
+    console.log("Check guess function")
+}
